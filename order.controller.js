1f8915a8c73c99b776fc50163db49e12eeac9be8
Abhishek Mishra <abhishek.mishra@wishfin.com>
2019-03-16 17:17:08 +0530

    changes done

--- order.controller.js
+++ order.controller.js
@@ -1,11 +1,11 @@
 const Order = require('./order.model.js');
+const User = require('./user.model.js');
 var jwt = require('jsonwebtoken');
 const Joi = require('joi');
 
 exports.create = (req, res) => {
     // Request validation
     const orderData = schema.validate(req.body);
-    console.log(orderData.error);
     if (orderData.error) {
         return res.status(403).send({
             result:orderData.error.ValidationError,
@@ -34,34 +34,59 @@ exports.create = (req, res) => {
 
 // Retrieve all products from the database.
 exports.findAll = (req, res) => {
-    Order.find()
+
+    Order.find({}).populate({
+        path: 'chapterid',
+        populate: {
+            path: 'subjectid',
+            populate: { path: 'classid', model: 'Addclass' }
+        }
+  });
+
+  Order.aggregate([{
+    $lookup: {
+        from: "Users", // collection name in db
+        localField: "user",
+        foreignField: "_id",
+        as: "usersData"
+    }
+}]).exec(function(err, orders) {
+    // students contain WorksnapsTimeEntries
+    if(err){
+        console.log(err);
+    }
+    res.send(orders);
+});
+
+/*
+    Order.find().populate('Users Products')
     .then(orders => {
         res.send(orders);
     }).catch(err => {
         res.status(500).send({
             message: err.message || "Something wrong while retrieving products."
         });
-    });
+    }); */
 };
 
 // Find a single product with a productId
 exports.findOne = (req, res) => {
-    User.findById(req.params.userId)
-    .then(user => {
-        if(!user) {
+    Order.findById(req.params.orderId)
+    .then(orders => {
+        if(!orders) {
             return res.status(404).send({
-                message: "user not found with id " + req.params.userId
+                message: "order not found with id " + req.params.orderId
             });            
         }
         res.send(User);
     }).catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
-                message: "User not found with id " + req.params.userId
+                message: "order not found with id " + req.params.orderId
             });                
         }
         return res.status(500).send({
-            message: "Something wrong retrieving user with id " + req.params.userId
+            message: "Something wrong retrieving user with id " + req.params.orderId
         });
     });
 };
@@ -69,65 +94,69 @@ exports.findOne = (req, res) => {
 // Update a product
 exports.update = (req, res) => {
     // Validate Request
-    if(!req.body) {
-        return res.status(400).send({
-            message: "user content can not be empty"
+    const orderData = schema.validate(req.body);
+    if (orderData.error) {
+        return res.status(403).send({
+            result:orderData.error.ValidationError,
+            message: "order content can not be empty"
         });
-    }
-
+      }
+    req.body = orderData.value;
     // Find and update product with the request body
-    User.findOneAndUpdate,(req.params.userId, {
-        name: req.body.name, 
-        age: req.body.age,
-        address: req.body.address,
+    Order.findOneAndUpdate,(req.params.orderId, {
+        product_name: req.body.product_name, 
+        user_id: req.body.user_id,
+        product_id: req.body.product_id,
+        order_method: req.body.order_method,
+        order_status: req.body.order_status
         
     }, {new: true})
-    .then(user => {
-        if(!user) {
+    .then(orders => {
+        if(!orders) {
             return res.status(404).send({
-                message: "user not found with id " + req.params.userId
+                message: "order not found with id " + req.params.orderId
             });
         }
-        res.send(user);
+        res.send(orders);
     }).catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
-                message: "Product not found with id " + req.params.userId
+                message: "order not found with id " + req.params.orderId
             });                
         }
         return res.status(500).send({
-            message: "Something wrong updating note with id " + req.params.userId
+            message: "Something wrong updating note with id " + req.params.orderId
         });
     });
 };
 
 // Delete a note with the specified noteId in the request
 exports.delete = (req, res) => { 
-    User.findOneAndDelete(req.params.userId)
-    .then(user => {
-        if(!user) {
+    Order.findOneAndDelete(req.params.orderId)
+    .then(orders => {
+        if(!orders) {
             return res.status(404).send({
-                message: "Product not found with id " + req.params.userId
+                message: "Product not found with id " + req.params.orderId
             });
         }
         res.send({message: "user deleted successfully!"});
     }).catch(err => {
         if(err.kind === 'ObjectId' || err.name === 'NotFound') {
             return res.status(404).send({
-                message: "user not found with id " + req.params.userId
+                message: "user not found with id " + req.params.orderId
             });                
         }
         return res.status(500).send({
-            message: "Could not delete user with id " + req.params.userId
+            message: "Could not delete user with id " + req.params.orderId
         });
     });
 };
-
+ 
 var schema = Joi.object().keys({
     product_name: Joi.string().min(3).max(50).required().label("Please Enter valid product Name"),
-    user_id: Joi.number().integer().required().label("Please Enter valid user id "),
-    product_id: Joi.number().integer().required().label("Please Enter valid user id "),
+    user_id: Joi.string().min(3).max(50).required().label("Please Enter valid user id "),
+    product_id: Joi.string().min(3).max(50).required().label("Please Enter valid user id "),
     order_method: Joi.string().min(2).max(10).required().label("Please Enter valid Order Method"),
-    order_status: Joi.string().min(2).max(6).required().label("Please Enter valid Order Status"),
+    order_status: Joi.string().min(2).max(10).required().label("Please Enter valid Order Status"),
 });
 
--- order.model.js
+++ order.model.js
@@ -5,7 +5,9 @@ const OrderSchema = mongoose.Schema({
     user_id: { type : String , required : true },
     product_id: {type : String , required : true},
     order_method: {type : String , required : true },
-    order_status:{type : String , required : true } 
+    order_status:{type : String , required : true },
+    user: [{type: mongoose.Schema.Types.ObjectId, ref: 'Users'}],
+    product: [{type: mongoose.Schema.Types.ObjectId, ref: 'Products'}]
 }, {
     timestamps: true
 });
